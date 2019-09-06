f15343a983d3289e39def780ea0fc2d7d180c127
XAmi101 <famousvacancy@gmail.com>
2019-09-06 10:00:08 -0400

    the testing is now working, fixed the json issue

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,9 +1,15 @@
-// import React from 'react';
+import React from 'react';
 // import ReactDOM from 'react-dom';
-// import App from './App';
-//
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+import { render } from '@testing-library/react';
+import App from './App';
+
+
+test('App renders without crashing', () => {
+  render(<App />);
+});
+
+test('contains scoreboard Title', () => {
+  const { getByText }  = render(<App />);
+  getByText("Lets Play Baseball!");
+
+});
